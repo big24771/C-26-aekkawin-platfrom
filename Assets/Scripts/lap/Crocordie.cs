84a7d897537e547be720fe66ad26327b263e29c0
big24771 <aekkawin.yimp@bumail.net>
2024-10-02 19:33:27 +0700

    big
    final

--- Assets/Scripts/lap/Crocordie.cs
+++ Assets/Scripts/lap/Crocordie.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class Crocordie : Ennamy
 {
+    [SerializeField]private float attackRange;
+    public Player player;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+
+    //colldown bullet
+    [SerializeField] private float bulletSpawnTime;
+    [SerializeField] private float bulletTime;
+
+    private void Update()
+    {
+        bulletTime -= Time.deltaTime;
+        Behaviour();
+    }
+
     public override void Behaviour()
     {
-        Debug.Log("Crocordie.Behaviour");
+        Vector3 direction = player.transform.position - transform.position;
+        float distance = direction.magnitude;
+        
+        if (distance < attackRange) 
+        {
+            Shoot();
+        
+        }
 
+    }
+    private void Shoot()
+    {
+        if (bulletTime <= 0)
+        {
+            Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+
+            bulletTime = bulletSpawnTime;
+        }
+    
     }
 }
